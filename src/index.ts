9589d9e1265ae9281ec6cf7b4ca11539e0bcc6f7
zhanba <c5e1856@gmail.com>
2019-08-23 22:59:57 +0800

    feat: add web  component

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+class ProjectLink extends HTMLElement {
+  constructor() {
+    super();
+    const template = document.getElementsByTagName('template')[0].content
+
+    const title = this.getAttribute("title");
+    const url = this.getAttribute("url");
+    const repo = this.getAttribute("repo");
+
+    this.attachShadow({ mode: "open" }).appendChild(template.cloneNode(true));
+    const links = this.shadowRoot!.querySelectorAll<HTMLAnchorElement>('.link')
+    links[0].textContent = title
+    links[0].href = url || ''
+    links[1].href = repo || ''
+  }
+}
+
+customElements.define("project-link", ProjectLink);
